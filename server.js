dc6102bd66a9c16c1f322be16936de81d73ec8f7
Anasvirat18 <factsmd95@gmail.com>
2026-04-23 13:44:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const https = require("https");
 const { exec } = require("child_process");
 
 const app = express();
@@ -12,10 +11,6 @@ const channels = {
   "1": "https://mumt02.tangotv.in/MATHRUBHUMINEWS/index.m3u8"
 };
 
-// GitHub raw watermark logo
-const remoteLogoUrl = "https://raw.githubusercontent.com/Anasvirat18/Hls/refs/heads/main/tbl.png";
-
-// Route
 app.get("/:id/:file?", (req, res) => {
   const id = req.params.id;
   const requestFile = req.params.file;
@@ -28,26 +23,10 @@ app.get("/:id/:file?", (req, res) => {
   const tmpDir = path.join(__dirname, id);
   const m3u8File = path.join(tmpDir, "stream.m3u8");
   const lockFile = path.join(tmpDir, "lock");
-  const logoFile = path.join(tmpDir, "logo.png");
 
   // Create folder
   if (!fs.existsSync(tmpDir)) {
     fs.mkdirSync(tmpDir, { recursive: true });
-  }
-
-  // Download logo once
-  if (!fs.existsSync(logoFile)) {
-    const file = fs.createWriteStream(logoFile);
-
-    https.get(remoteLogoUrl, (response) => {
-      response.pipe(file);
-      file.on("finish", () => {
-        file.close();
-        console.log("Logo downloaded");
-      });
-    }).on("error", (err) => {
-      console.error("Logo download failed:", err.message);
-    });
   }
 
   // Start FFmpeg if needed
@@ -60,17 +39,17 @@ app.get("/:id/:file?", (req, res) => {
     shouldStart = true;
   } else {
     const last = fs.statSync(lockFile).mtimeMs;
-    if ((now - last) / 1000 > 25) {
+    if ((now - last) / 1000 > 20) {
       shouldStart = true;
     }
   }
 
   if (shouldStart) {
-    const cmd = `ffmpeg -loglevel error -y \
--i "${inputUrl}" -i "${logoFile}" \
--filter_complex "[1]crop='min(iw,ih)':'min(iw,ih)',scale=80:80,format=rgba,colorchannelmixer=aa=0.5[wm];[0][wm]overlay=x=main_w-overlay_w-20:y=main_h-overlay_h-20" \
--c:v libx264 -preset veryfast -crf 23 -c:a aac -b:a 128k \
--f hls -hls_time 4 -hls_list_size 6 \
+    const cmd = `./ffmpeg -loglevel error -y \
+-headers "Referer: https://tangotv.in\\r\\nUser-Agent: Mozilla/5.0\\r\\n" \
+-i "${inputUrl}" \
+-c copy \
+-f hls -hls_time 2 -hls_list_size 4 \
 -hls_flags delete_segments+append_list+omit_endlist \
 -hls_segment_filename "${tmpDir}/seg_%03d.ts" \
 "${m3u8File}" > "${tmpDir}/log.txt" 2>&1 &`;
